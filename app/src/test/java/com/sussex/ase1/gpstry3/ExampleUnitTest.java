07344ef1911cc1135fbb166c292b4232e7459b32
Agne Kovalkovaite <ak435@m011397.inf.susx.ac.uk>
2016-11-10 18:39:09 +0000

    test1

--- app/src/test/java/com/sussex/ase1/gpstry3/ExampleUnitTest.java
+++ app/src/test/java/com/sussex/ase1/gpstry3/ExampleUnitTest.java
@@ -9,9 +9,13 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        MainActivity myUnit = new MainActivity();
+
+        assertTrue (myUnit.validPostcode("QN1 9PE"));
     }
 }
